c223cb7d242f31b8c2da60b3f378ee8b61aa4863
FelixWodaczek <f.wodaczek@gmail.com>
2022-07-28 17:46:42 +0200

    add prototype of solver
    define system of equations
    provide ability to produce constraints
    add simple test cases

--- py_src/mk_modelling.py
+++ py_src/mk_modelling.py
@@ -1,16 +1,76 @@
 import numpy as np
 import scipy as sp
+from scipy.integrate import ode
+from scipy.constants import Boltzmann
 
 class MkModeller:
     def __init__(self):
-        pass
+        self.diffusion = np.zeros((9, ), dtype=np.float64)
+        self.k = np.zeros((9, 2), dtype=np.float64)
+
+        self.y_mask = np.empty((9, ), np.float64)
+        self.y_mask[0] = 1.
+        self.y_mask[6] = 0.1
+        self.y_mask[8] = 0.1
+        self.mask_cond = np.logical_not(np.isnan(self.y_mask))
+
+        self.y_inputs = np.zeros((9, ), np.float64)
+
+    def init_mask_cond(self):
+        self.mask_cond = np.logical_not(np.isnan(self.y_mask))
 
     @staticmethod
-    def k_from_energy():
-        pass
+    def k_from_energy(temperature, energy=None, delta_h=None, delta_s=None):
+        if energy is None:
+            return np.exp(-delta_h/(temperature * Boltzmann)) * np.exp(delta_s/Boltzmann)
+        else:
+            return np.exp(-energy/(temperature * Boltzmann))
 
-    def build_system_of_equations():
-        pass
+
+    def system_of_equations(self, t, y):
+        self.diffusion[:] = 0.
+
+        star = 1 - np.sum(y[1:6]) - y[7] # TODO: check if this is actually correct
+        self.diffusion[0] = self.k[0, 0] * y[1] - self.k[0, 1] * y[0] 
+        self.diffusion[1] = self.k[0, 1] * y[0] - self.k[1, 1] * y[1] + self.k[1, 0] * y[2] - self.k[2, 1] * y[1] + self.k[2, 0] * y[3] - self.k[0, 0] * y[1]
+        self.diffusion[2] = self.k[1, 1] * y[1] - self.k[1, 0] * y[2] + self.k[3, 0] * y[4] - self.k[3, 1] * y[2]
+        self.diffusion[3] = self.k[2, 1] * y[1] - self.k[2, 0] * y[2] + self.k[4, 0] * y[4] - self.k[4, 1] * y[3]
+        self.diffusion[4] = self.k[3, 1] * y[2] + self.k[4, 1] * y[3] + self.k[5, 0] * y[5] * y[6] - self.k[3, 0] * y[4] - self.k[4, 0] * y[4] - self.k[5, 1] * y[4]
+        self.diffusion[5] = self.k[5, 1] * y[4] + self.k[6, 0] * y[7] - self.k[5, 0] * y[5] * y[6] - self.k[6, 1] * y[5]
+        self.diffusion[6] = self.k[5, 1] * y[4] - self.k[5, 0] * y[5] * y[6]
+        self.diffusion[7] = self.k[6, 1] * y[5] - self.k[7, 1] * y[7] - self.k[6, 0] * y[7] + self.k[7, 0] * star * y[8]
+        self.diffusion[8] = self.k[7, 1] * y[7] - self.k[7, 0] * star * y[8]
+
+        return self.diffusion
+
     
-    def solve():
-        pass
+    def constrained_soe(self, t, y):
+        self.y_inputs[:] = np.where(self.mask_cond, self.y_mask, y)
+        
+        self.system_of_equations(t, self.y_inputs)
+        self.diffusion[self.mask_cond] = 0.
+        
+        return self.diffusion
+
+
+    def solve(self, n_steps, y_0=None, target_function=None):
+        self.init_mask_cond()
+
+        if y_0 is None:
+            y_0 = np.zeros((9, ), dtype=np.float64)
+            y_0[self.mask_cond] = self.y_mask[self.mask_cond]
+
+        if target_function is None:
+            target_function = self.constrained_soe
+
+        solver = ode(target_function).set_integrator('vode', method='adams')
+        solver.set_initial_value(y=y_0, t=0)
+        
+        solution = np.zeros((n_steps, y_0.shape[0]))
+        for ii_step, step in enumerate(np.arange(1, n_steps+1)):
+            solution[ii_step, :] = solver.integrate(step)
+
+        return solution
+
+if __name__ == '__main__':
+    pass
--- unittests/test_mk_modelling.py
+++ unittests/test_mk_modelling.py
@@ -1,11 +1,53 @@
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "py_src/"))
 
+from mk_modelling import MkModeller
+import numpy as np
+from numpy.random import default_rng
 import unittest
+import matplotlib.pyplot as plt
 
 class TestMkModelling(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        self.rng = default_rng(34875223)
+    
+    def test_dimensionality(self):
+        n_timesteps = 100
+        mk_modeller = MkModeller()
+        mk_modeller.y_mask[:] = np.nan
+
+        sol = mk_modeller.solve(n_timesteps)
+        
+        assert sol.shape[0] == n_timesteps
+        assert sol.shape[1] == 9
+        assert np.allclose(sol, 0.), "Did not stay all 0"
+
+    def test_constraints(self):
+        n_timesteps = 100
+        mk_modeller = MkModeller()
+        mk_modeller.y_mask[:] = 1.
+
+        mk_modeller.y_mask[:] = 1.
+        sol = mk_modeller.solve(n_timesteps)
+
+        assert sol.shape[0] == n_timesteps
+        assert sol.shape[1] == 9
+        assert np.allclose(sol, 1.), "Did not stay all 1"
+
+        for ii_test in range(9):
+            mk_modeller = MkModeller()
+            mk_modeller.y_mask[:] = np.nan 
+            mk_modeller.y_mask[ii_test] = 30.
+
+            sol = mk_modeller.solve(30)
+
+            assert np.allclose(sol[:, ii_test]) == 30., "Not close to 30 in %u."%ii_test
+
+    def test_equilibria(self):
+        for ii_test in range(9):
+            pass # TODO check proper implementation of all k values
 
 if __name__ == '__main__':
     unittest.main()
